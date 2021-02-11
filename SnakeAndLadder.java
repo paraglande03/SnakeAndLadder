cc1caaf1448903e61877e383f7b8e1d0c6199404
paraglande03 <paraglande03@gmail.com>
2021-02-11 16:51:04 +0530

    Add Dice Rolling

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,12 +1,26 @@
 import com.sun.javaws.Main;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 
+    // Intializing variables
     static final int STARTNG_POSITION=0;
+    static final int PLAYER=1;
+    static Random RANDOM  = new Random();
+
+    //Rolling Dice Method
+    public  void diceRoll(){
+
+        int DICE= (RANDOM.nextInt(6)+1);
+        System.out.println("Dice Number="+DICE);
+    }
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to the game");
+        SnakeAndLadder snakeAndLadder= new SnakeAndLadder();
+        snakeAndLadder.diceRoll();
 
 
     }
