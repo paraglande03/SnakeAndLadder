5b45995f3759e7a55a2d463f1361cf81c38fedce
paraglande03 <paraglande03@gmail.com>
2021-02-11 16:37:46 +0530

    Add Initializing and starting position

---
+++ SnakeAndLadder.java
@@ -0,0 +1,15 @@
+import com.sun.javaws.Main;
+
+public class SnakeAndLadder {
+
+    static final int STARTNG_POSITION=0;
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the game");
+
+
+    }
+
+
+}
