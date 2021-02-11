911121ca7eee3a11c4a06c4731d88494ae6e04f0
paraglande03 <paraglande03@gmail.com>
2021-02-11 17:30:56 +0530

    Add condition for winning

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,6 +4,7 @@ public class SnakeAndLadder {
 
     // Intializing variables
     static final int STARTNG_POSITION=0;
+    static final int ENDING_POSITION=100;
     static final int PLAYER=1;
     static final int LADDER=1;
     static final int SNAKE=2;
@@ -22,28 +23,33 @@ public class SnakeAndLadder {
     //Gameplay method
     public void gameplay() {
         int playerPosition = STARTNG_POSITION;
-        int dice = diceRoll();
-        int action = RANDOM.nextInt(3);
-        System.out.println("Action Taken--" + action);
-        switch (action) {
-            case LADDER:
-                playerPosition = playerPosition + dice;
-                System.out.println("Player Position  " + playerPosition);
-                break;
-            case SNAKE:
-                playerPosition = playerPosition - dice;
-                if (playerPosition < 0) {
-                    playerPosition = STARTNG_POSITION;
-                }
-                System.out.println("Player Position  " + playerPosition);
-                break;
-            case NO_PLAY:
-                playerPosition = playerPosition;
-                System.out.println("same position as before " + playerPosition);
-                break;
+
+         //Adding while for ending position
+        while (playerPosition < ENDING_POSITION) {
+
+            int dice = diceRoll();
+            int action = RANDOM.nextInt(3);
+            System.out.println("Action Taken--" + action);
+            switch (action) {
+                case LADDER:
+                    playerPosition = playerPosition + dice;
+                    System.out.println("Player Position  " + playerPosition);
+                    break;
+                case SNAKE:
+                    playerPosition = playerPosition - dice;
+                    if (playerPosition < 0) {
+                        playerPosition = STARTNG_POSITION;
+                    }
+                    System.out.println("Player Position  " + playerPosition);
+                    break;
+                case NO_PLAY:
+                    playerPosition = playerPosition;
+                    System.out.println("same position as before i.e " + playerPosition);
+                    break;
             }
 
         }
+    }
 
         public static void main (String[]args ){
 
